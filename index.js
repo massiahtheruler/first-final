d723d51092560342ab032cd44a991749dd0adc7c
Christine Marquez <justin.henry124@gmail.com>
2026-03-19 17:15:01 -0400

    notes revised

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+//index.js
+
 function openMenu() {
+   
+   
     document.body.classList += " menu--open"
 }
 
 function closeMenu() {
     document.body.classList.remove('menu--open')
 }
+const menuToggleBtn = document.getElementsByClassName('menu__btn')
+const closeMenuBtn = document.getElementsByClassName('btn__menu--close')
+const body = document.body;
+const html = document.documentElement; 
+
+function toggleMenu() {
+    body.classList.toggle('menu-open');
+    html.classList.toggle('no-scroll')
+    body.classList.toggle('no-scroll')
+}
+
+menuToggleBtn.addEventListener('click, toggleMenu')
+closeMenuBtn.addEventListener('click, toggleMenu')
